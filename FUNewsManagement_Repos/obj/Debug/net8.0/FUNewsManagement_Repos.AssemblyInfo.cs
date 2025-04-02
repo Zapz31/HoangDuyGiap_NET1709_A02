154f2088d8f1f5a744a886dc862598435a144260
Zapz31 <hgiap46@gmail.com>
2025-04-02 22:29:22 +0700

    Update project structure and add missing user ID retrieval in NewsArticlePage

--- FUNewsManagement_Repos/obj/Debug/net8.0/FUNewsManagement_Repos.AssemblyInfo.cs
+++ FUNewsManagement_Repos/obj/Debug/net8.0/FUNewsManagement_Repos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUNewsManagement_Repos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a8e6712e316d51d374e28ee4ade3dc08d94f01")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUNewsManagement_Repos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUNewsManagement_Repos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
